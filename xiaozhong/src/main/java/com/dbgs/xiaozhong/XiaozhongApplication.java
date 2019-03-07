644f0959de8b64c377e74cc5ecd6a33e4eb236a0
demozhang <zhang_yi_chang@163.com>
2019-03-07 11:24:12 +0800

    第一次提交

---
+++ xiaozhong/src/main/java/com/dbgs/xiaozhong/XiaozhongApplication.java
@@ -0,0 +1,13 @@
+package com.dbgs.xiaozhong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaozhongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaozhongApplication.class, args);
+    }
+
+}
---
+++ xiaozhong/src/main/java/com/dbgs/xiaozhong/controller/HelloDemo.java
@@ -0,0 +1,13 @@
+package com.dbgs.xiaozhong.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloDemo {
+    @RequestMapping("/ni")
+    public String tt(){
+        return "ni hao";
+    }
+}
