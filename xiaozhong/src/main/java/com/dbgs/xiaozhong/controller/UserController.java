fd8d080be4548b1c64d5171be66c4b42152b023d
demozhang92 <48115599+demozhang92@users.noreply.github.com>
2019-03-07 12:50:46 +0800

    Update UserController.java

--- xiaozhong/src/main/java/com/dbgs/xiaozhong/controller/UserController.java
+++ xiaozhong/src/main/java/com/dbgs/xiaozhong/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @RequestMapping("/get")
     public String gett(){
         System.out.println("你好啊！");
     }
